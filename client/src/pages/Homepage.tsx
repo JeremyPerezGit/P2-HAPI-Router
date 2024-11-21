469399eae80402e95084c0f9804bbc1e62ad49e4
Vincent CHING <chingvincentdu20@yahoo.fr>
2024-11-21 15:55:15 +0100

    [FEAT] fix bug

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -1,15 +1,8 @@
-import React, { useState, useEffect } from "react";
-import trainImage from '../images/train.png'; // Ajustez le chemin vers votre image
-import "./Homepage.css"
-
+import { useEffect, useState } from "react";
+import trainImage from "../images/train.png"; // Ajustez le chemin vers votre image
+import "./Homepage.css";
 
 function Homepage() {
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
-
   const texts = [
     "Expelliarmus!",
     "Wingardium Leviosa!",
@@ -18,7 +11,7 @@ function Homepage() {
     "Lumos!",
     "Nox!",
     "Accio!",
-    "Alohomora!"
+    "Alohomora!",
   ];
 
   const [visible, setVisible] = useState(true);
@@ -33,47 +26,54 @@ function Homepage() {
 
   return (
     <div className="homepage">
-      <header className="header">
-        <nav className="navbar">
-          <div className={`burger-menu ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}>
-            <div className="burger-bar"></div>
-            <div className="burger-bar"></div>
-            <div className="burger-bar"></div>
-          </div>
-          <div className={`menu ${menuOpen ? 'open' : ''}`}>
-            <a href="#section1">Home</a>
-            <a href="#section2">Characteres</a>
-            <a href="#section4">Spells</a>
-          </div>
-        </nav>
-      </header>
       <div className="content">
         <section className="section section1" id="section1">
           <h2> HARRY POTTER UNIVERSE</h2>
           <p>Learn more about your favorite characters</p>
-          <img src={"https://ik.imagekit.io/hpapi/harry.jpg"} alt="Main Character" className="character-image" />
-          <button className="center-button">Personnages</button>
+          <img
+            src={"https://ik.imagekit.io/hpapi/harry.jpg"}
+            alt="Main Character"
+            className="character-image"
+          />
+          <button type="button" className="center-button">
+            Personnages
+          </button>
         </section>
         <section className="section section2" id="section2">
           <h2>What would magic be without spells?</h2>
           <p> Venez découvrir les sorts de l’univers Harry Potter !</p>
-          <div className={`random-text-container ${visible ? 'visible' : 'hidden'}`}>
-            {texts.map((text, index) => (
-              <span key={index} className="random-text">{text}</span>
+          <div
+            className={`random-text-container ${visible ? "visible" : "hidden"}`}
+          >
+            {texts.map((text) => (
+              <span key={text} className="random-text">
+                {text}
+              </span>
             ))}
           </div>
-          <button className="center-button">Sorts</button>
+          <button type="button" className="center-button">
+            Sorts
+          </button>
         </section>
         <section className="section section3" id="section3">
           <h2>Useless information therefore essential:</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio ratione adipisci ab, amet quas eligendi odio, nostrum autem hic nihil molestias iste maxime, voluptas et mollitia tempora impedit placeat veritatis sint. Suscipit architecto, necessitatibus aliquid soluta, doloribus fugiat neque error libero voluptates ducimus veritatis perferendis. Molestias quod odio ex eaque?</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
+            ratione adipisci ab, amet quas eligendi odio, nostrum autem hic
+            nihil molestias iste maxime, voluptas et mollitia tempora impedit
+            placeat veritatis sint. Suscipit architecto, necessitatibus aliquid
+            soluta, doloribus fugiat neque error libero voluptates ducimus
+            veritatis perferendis. Molestias quod odio ex eaque?
+          </p>
         </section>
         <section className="section section4" id="section4">
           <div className="train-container">
             <img src={trainImage} alt="Train" className="train-image" />
             <div className="text-overlay">
               <h2 className="train-title">You want to test your knowledge?</h2>
-              <button className="center-button2">Start the quiz</button>
+              <button type="button" className="center-button2">
+                Start the quiz
+              </button>
             </div>
           </div>
         </section>
